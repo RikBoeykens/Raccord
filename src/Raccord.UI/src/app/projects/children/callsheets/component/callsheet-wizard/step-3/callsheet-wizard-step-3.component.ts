0ba36c0adf512212f15e09b6bc5ed589cc9f945e
RikBoeykens <rikboeykens@gmail.com>
2017-07-19 07:58:07 +0200

    callsheets - step 3 - update characters

--- src/Raccord.UI/src/app/projects/children/callsheets/component/callsheet-wizard/step-3/callsheet-wizard-step-3.component.ts
+++ src/Raccord.UI/src/app/projects/children/callsheets/component/callsheet-wizard/step-3/callsheet-wizard-step-3.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { CallsheetSceneHttpService } from "../../../children/callsheet-scenes/service/callsheet-scene-http.service";
+import { CallsheetSceneCharacterHttpService } from "../../../children/callsheet-scene-characters/service/callsheet-scene-character-http.service";
 import { LoadingService } from '../../../../../../loading/service/loading.service';
 import { DialogService } from '../../../../../../shared/service/dialog.service';
 import { ProjectSummary } from '../../../../../model/project-summary.model';
@@ -22,6 +23,7 @@ export class CallsheetWizardStep3Component implements OnInit {
         private _route: ActivatedRoute,
         private _router: Router,
         private _callsheetSceneHttpService: CallsheetSceneHttpService,
+        private _callsheetSceneCharacterHttpService: CallsheetSceneCharacterHttpService,
         private _loadingService: LoadingService,
         private _dialogService: DialogService
     ) {
@@ -42,6 +44,48 @@ export class CallsheetWizardStep3Component implements OnInit {
             this.scenes = data.map((scene)=> new CallsheetSceneWrapper(scene));
             this._loadingService.endLoading(loadingId);
         });
+    }
+
+    goToNextStep(){
+
+    }
+
+    updateCharacterLink(character: CallsheetSceneCharacterWrapper, callsheetSceneId: number){
+        if(character.isLinked&&character.callsheetSceneCharacterId){
+            this.removeScheduleCharacterLink(character);
+        }else{
+            this.addScheduleCharacterLink(character, callsheetSceneId);
+        }
+    }
+
+    removeScheduleCharacterLink(character: CallsheetSceneCharacterWrapper){
+        let loadingId = this._loadingService.startLoading();
+
+        this._callsheetSceneCharacterHttpService.removeLink(character.callsheetSceneCharacterId).then(data=>{
+            if(typeof(data)=='string'){
+                this._dialogService.error(data);
+            }else{
+                this.getScenes();
+            }
+        }).catch()
+        .then(()=>
+            this._loadingService.endLoading(loadingId)
+        );
+    }
+
+    addScheduleCharacterLink(character: CallsheetSceneCharacterWrapper, callsheetSceneId: number){
+        let loadingId = this._loadingService.startLoading();
+
+        this._callsheetSceneCharacterHttpService.addLink(callsheetSceneId, character.linkID).then(data=>{
+            if(typeof(data)=='string'){
+                this._dialogService.error(data);
+            }else{
+                this.getScenes();
+            }
+        }).catch()
+        .then(()=>
+            this._loadingService.endLoading(loadingId)
+        );
     }
 }
 
