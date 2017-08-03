a1535427c9ca53ee305a5e190d1a77837d053713
RikBoeykens <rikboeykens@gmail.com>
2017-08-03 22:59:10 +0200

    crew-users - remove crew from project

--- src/Raccord.UI/src/app/admin/projects/component/admin-project-landing/admin-project-landing.component.ts
+++ src/Raccord.UI/src/app/admin/projects/component/admin-project-landing/admin-project-landing.component.ts
@@ -4,6 +4,8 @@ import { AdminProjectHttpService } from '../../service/admin-project-http.servic
 import { FullProject } from '../../../../projects';
 import { CrewUserUser } from "../../../crew/model/crew-user-user.model";
 import { AdminCrewHttpService } from "../../../crew/service/admin-crew-http.service";
+import { LoadingService } from "../../../../loading/service/loading.service";
+import { DialogService } from "../../../../shared/service/dialog.service";
 
 @Component({
     templateUrl: 'admin-project-landing.component.html',
@@ -16,6 +18,8 @@ export class AdminProjectLandingComponent {
     constructor(
         private _projectHttpService: AdminProjectHttpService,
         private _crewHttpService: AdminCrewHttpService,
+        private _loadingService: LoadingService,
+        private _dialogService: DialogService,
         private route: ActivatedRoute,
         private router: Router
     ){
@@ -26,5 +30,36 @@ export class AdminProjectLandingComponent {
             this.project = data.project;
             this.crew = data.crew;
         });
+    }
+
+    getCrewMembers(){
+        let loadingId = this._loadingService.startLoading();
+
+        this._crewHttpService.getUsers(this.project.id).then(data=>{
+            if(typeof(data)=='string'){
+                this._dialogService.error(data);
+            }else{
+                this.crew = data;
+            }
+        }).catch()
+        .then(()=>
+            this._loadingService.endLoading(loadingId)
+        );
+    }
+
+    removeCrewMember(crewUser: CrewUserUser){
+        let loadingId = this._loadingService.startLoading();
+
+        this._crewHttpService.delete(crewUser.id).then(data=>{
+            if(typeof(data)=='string'){
+                this._dialogService.error(data);
+            }else{
+                this._dialogService.success("Successfully removed crew member.");
+                this.getCrewMembers();
+            }
+        }).catch()
+        .then(()=>
+            this._loadingService.endLoading(loadingId)
+        );
     }
 }
