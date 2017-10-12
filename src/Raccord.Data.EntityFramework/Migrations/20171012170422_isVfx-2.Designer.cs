5d5a647aec681a3124369777a1e27e826c49d720
RikBoeykens <rikboeykens@gmail.com>
2017-10-12 23:09:17 +0200

    slates - fix IsVfx issues

--- src/Raccord.Data.EntityFramework/Migrations/20171012170422_isVfx-2.Designer.cs
+++ src/Raccord.Data.EntityFramework/Migrations/20171012170422_isVfx-2.Designer.cs
@@ -9,9 +9,10 @@ using Raccord.Core.Enums;
 namespace Raccord.Data.EntityFramework.Migrations
 {
     [DbContext(typeof(RaccordDBContext))]
-    partial class RaccordDBContextModelSnapshot : ModelSnapshot
+    [Migration("20171012170422_isVfx-2")]
+    partial class isVfx2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.0.3");
