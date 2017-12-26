9bc25ae27f425d6e7006549e97ddad85f19e26ff
Rik <rbk@foreach.be>
2017-12-26 16:50:39 +0100

    heroku-poc - update deploy script

--- src/Raccord.UI/DeployScript.sh
+++ src/Raccord.UI/DeployScript.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+npm run build:prod
 dotnet publish -c Release
 docker build -t raccord-ui-poc ./bin/release/netcoreapp1.0/publish
 docker tag raccord-ui-poc registry.heroku.com/raccord-ui-poc/web
